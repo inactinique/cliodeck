ae02f94724c3e1b384e2651eec07a7212dadffb4
inactinique <frederic.clavert@uni.lu>
2026-02-09 07:26:21 +0100

    move to cliodeck account

--- src/main/services/topic-modeling-service.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Topic Modeling Service Singleton
- * Provides access to the TopicModelingService instance
- */
-import { TopicModelingService } from '../../../backend/core/analysis/TopicModelingService.js';
-
-export const topicModelingService = new TopicModelingService();
--- src/renderer/src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ElectronAPI } from '../../../preload';
-
-declare global {
-  interface Window {
-    electron: ElectronAPI;
-  }
-}
-
-export {};
